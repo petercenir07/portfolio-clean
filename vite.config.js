6a5ba500478d5bfce1110c93f520b267de4ccf39
Peter Cenir <peter.cenir@gmail.com>
2025-07-14 11:18:00 +0800

    Updated PDF file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio-site/',
+  base: '/portfolio-clean/', // ✅ Must match your GitHub repo name exactly
   plugins: [react()],
 })
